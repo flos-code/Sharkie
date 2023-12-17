f673910b264035189a9808d218d4feed44bf8a93
flos-code <148456982+flos-code@users.noreply.github.com>
2023-12-17 11:49:35 +0100

    fix height on mobile

--- js/games.js
+++ js/games.js
@@ -386,4 +386,19 @@ function handleUserDevice() {
     }
 
   }
+
+
+  function checkOrientation() {
+    if (window.matchMedia("(orientation: landscape)").matches) {
+      if (window.innerHeight < 480) {
+        newHeight = window.innerHeight;
+        document.getElementById('canvas').style.height = `${newHeight}px`;
+      }
+    }
+    else {
+      document.getElementById('canvas').style.height = `100%`;
+    }
+  }
+
+
 }
